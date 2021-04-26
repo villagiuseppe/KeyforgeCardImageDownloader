bf1ea39f0880af41655906535cb4445a9d76c3cb
Giuseppe Villa <giuseppe.villa@gmail.com>
2021-04-26 22:12:14 +0200

    Initial commit

---
+++ src/KeyforgeCardImageDownloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KeyforgeCardImageDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
